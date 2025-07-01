9263c069ae9bb8a6c464998eafe48e354fa10527
Huryu-MDA <huryu@mdanderson.org>
2025-07-01 15:54:23 -0500

    subcode using in the steps of Mu2_2ndCall internally was added.

---
+++ somaticMutDetectTools/code/Mu2_2ndCall/subcode_for_filtering/NullReadtoZeroReplace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys, os
+
+#file = "BcfQuery_Biallelic_SampleMerged_VCFs.vcf"
+#Read = open(file)
+Read = sys.stdin
+
+for line in Read:
+    line = line.replace(",.\t", ",0\t")
+    line = line.replace(".\t", "0,0\t")
+    line = line.replace(".,", "0,")
+    print(line, end = "")
+    #break
